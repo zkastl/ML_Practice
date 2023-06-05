479db3272dc6f46b20c56ae92ab29c21f4c737d6
Zak Kastl <zkastl8@gmail.com>
2023-06-05 13:28:55 -0500

    added the quickstart tutorial

--- tutorial.py
+++ tutorial.py
@@ -1,1 +1,23 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
 print("Hello world!")
+
+# download training data from open datasets
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+# download test data from open datasets
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
