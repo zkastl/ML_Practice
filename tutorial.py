b41d82279c1f32c81ce0388f6306da9f052e5a23
Zak Kastl <zkastl8@gmail.com>
2023-06-15 17:00:31 -0500

    updated sd code

--- tutorial.py
+++ tutorial.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 model = keras_cv.models.StableDiffusionV2(img_width=512, img_height=512)
 
-images = model.text_to_image("photograph of an astronaut riding a horse", batch_size=3)
+images = model.text_to_image("cartoon of a dinosaur fighting a kitten", batch_size=3)
 
 def plot_images(images):
     plt.figure(figsize=(20, 20))
@@ -13,5 +13,5 @@ def plot_images(images):
         ax = plt.subplot(1, len(images), i + 1)
         plt.imshow(images[i])
         plt.axis("off")
-
+        plt.savefig(f'fig{i}.png')
 plot_images(images)
