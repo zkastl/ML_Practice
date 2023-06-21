dffd4bcb1f9f2305c1e839ea586d60a865ee308c
Zak Kastl <zkastl8@gmail.com>
2023-06-21 11:42:06 -0500

    more updates

--- DeepLearningPythonBook/imdb.py
+++ DeepLearningPythonBook/imdb.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 from keras import utils as kutils
 from keras.layers import Dense, Embedding, Flatten
 from keras.models import Sequential
+from keras.preprocessing.text import Tokenizer
 
 
 maxlen = 100
@@ -14,7 +15,22 @@ max_words = 10000
 log_dir = '.'
 tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir, histogram_freq=1)
 imdb_dir = '../data/aclImdb'
-train_dri = os.path.join(imdb_dir, 'train')
+train_dir = os.path.join(imdb_dir, 'train')
 
 labels = []
 texts = []
+
+for label_type in ['neg', 'pos']:
+    dir_name = os.path.join(train_dir, label_type)
+    for fname in os.listdir(dir_name):
+        if fname[-4:] == '.txt':
+            with open(os.path.join(dir_name, fname)) as f:
+                texts.append(f.read())
+
+            if label_type == 'neg':
+                labels.append(0)
+            else:
+                labels.append(1)
+
+tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts(texts)
