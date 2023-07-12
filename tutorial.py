e1578f9582a354503987f80d95296b393003e3b4
Zak Kastl <zkastl8@gmail.com>
2023-07-12 17:12:11 -0500

    more change, looking into sd more, data

--- tutorial.py
+++ tutorial.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 
 model = keras_cv.models.StableDiffusionV2(img_width=500, img_height=500)
 
-images = model.text_to_image("art of the blessed sacrament", batch_size=3)
+images = model.text_to_image("a picture of a card from the game Magic the Gathering", batch_size=4)
 
 def plot_images(images):
     plt.figure(figsize=(20, 20))
     for i in range(len(images)):
-        ax = plt.subplot(1, len(images), i + 1)
+        ax = plt.subplot(2, 2, i + 1)
         plt.imshow(images[i])
         plt.axis("off")
-        plt.savefig(f'fig{i}.png')
+        plt.savefig(f'fig{i}.png', bbox_inches='tight')
 plot_images(images)
